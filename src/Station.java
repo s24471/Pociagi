9d6fef7a2d1f6f5917647380c852e8a814ee3c76
s24471 <s24471@pjwstk.edu.pl>
2023-04-19 17:15:40 +0200

    zawieź nas do warszawy

--- src/Station.java
+++ src/Station.java
@@ -6,40 +6,20 @@ import java.util.Map;
 public class Station {
     String name;
     Map<Station, Road> roads;
-    ArrayList<Train> trains;
-    boolean alive;
-    Thread logic;
-
-    public Station(String name, Map<Station, Double> n) {
+    public Station(String name, Map<Station, Integer> n) {
         this.name = name;
-        alive = true;
-        trains = new ArrayList<>();
         roads = new HashMap<>();
         for(Station station: n.keySet()){
-            roads.put(station, new Road(this, station, n.get(station)));
-        }
-        logic = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Station.this.run();
+            if(station.roads.containsKey(this)){
+                roads.put(station, station.roads.get(this));
             }
-        });
-        logic.start();
+            else{
+                roads.put(station, new Road(this, station, n.get(station)));
+                station.roads.put(this, roads.get(station));
+            }
+        }
+
     }
 
-    public void run() {
-        while(alive){
-            try{
-                wait();
-            }catch (InterruptedException e){
-                ArrayList<Train> left = new ArrayList<>(trains);
-                for(Train train: left){
-                    if(roads.get(train.nextStop).free){
-                        train.next(roads.get(train.nextStop));
-                        trains.remove(train);
-                    }
-                }
-            }
-        }
-    }
+
 }
--- src/Train.java
+++ src/Train.java
@@ -1,16 +1,37 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Train {
+    public static int ID =0;
+    public int id;
     ArrayList<Station> path;
     Station nextStop;
     boolean ready;
     Thread logic;
     double toDrive;
     double left;
-
-    public Train(Station start, Station fin) {
+    boolean alive;
+    String name;
+    int maxCart;
+    int maxPull;
+    int maxElectric;
+    int progress;
+    int speed;
+    Road curr;
+    public Train(Station start, Station fin, int next, int speed, String name, int maxCart, int maxPull, int maxElectric) {
+        id=++ID;
+        alive = true;
+        this.speed = speed;
+        this.name = name;
+        this.maxCart = maxCart;
+        this.maxPull = maxPull;
+        this.maxElectric = maxElectric;
+        ready = false;
         path = Main.findRoute(start, fin);
-        nextStop = path.get(1);
+        progress = next;
+        nextStop = path.get(next+1);
         logic = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -18,14 +39,59 @@ public class Train {
             }
         });
         logic.start();
+        System.out.println(name +" wystartowal z " + path.get(progress).name);
+        path.get(progress).roads.get(nextStop).arrive(this);
     }
 
     public void next(Road road){
+        curr = road;
         ready = true;
-        toDrive = road.change(this);
+        toDrive = road.length;
         left = toDrive;
+        logic.interrupt();
+    }
+    public void finished(){
+        System.out.println("Dojechano do " + nextStop.name);
+        ready = false;
+        curr.finished();
+        left = 0;
+        progress++;
+        if(progress == path.size()-1){
+            Collections.reverse(path);
+            progress = 0;
+            sleep(28);
+        }
+        nextStop = path.get(progress+1);
+        sleep(2);
+        path.get(progress).roads.get(nextStop).arrive(this);
     }
     public void run(){
-
+        while(alive){
+            synchronized (this) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    if (ready) {
+                        while (left > 0) {
+                            drive();
+                            sleep(1);
+                        }
+                        finished();
+                    }
+                }
+            }
+        }
+    }
+    public void sleep(int n){
+        try {
+            TimeUnit.SECONDS.sleep(n);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void drive(){
+        left -= speed;
+        double factor = new Random().nextBoolean() ? 1.03 : 0.97;
+        speed = (int) Math.round(speed * factor);
     }
 }
